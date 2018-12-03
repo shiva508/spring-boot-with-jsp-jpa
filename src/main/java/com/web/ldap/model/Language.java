36528ec4d26656a26c1005337b74f363e412fec6
shiva <ram@ram-desktop>
2018-12-03 14:26:04 +0530

    vcbx

--- src/main/java/com/web/ldap/model/Language.java
+++ src/main/java/com/web/ldap/model/Language.java
@@ -1,6 +1,10 @@
 package com.web.ldap.model;
 
+import javax.annotation.Generated;
+import javax.persistence.GeneratedValue;
+
 public class Language {
+
 private Integer languageId;
 private String languageName;
 }
